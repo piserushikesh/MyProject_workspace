c48bb58490da434f6fbd68f536d4fd9e70c663da
InspiredEnigma12477 <sakoreshivam3901@gmail.com>
2023-02-28 00:16:09 +0530

    added ProjectInfo

--- stater/src/main/java/com/pojos/ProjectInfo.java
+++ stater/src/main/java/com/pojos/ProjectInfo.java
@@ -1,5 +1,31 @@
 package com.pojos;
 
-public class ProjectInfo {
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Blob;
+
+@Entity
+@Table(name = "ProjectInfo")
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class ProjectInfo extends BaseEntity{
+    @Column(name = "ProjectTitle", length = 20)
+    private String title;
+    @Column(name = "Description", length = 20)
+    private String description;
+    @Column(name = "SRS", length = 20)
+    @Lob
+    private Blob srs;
+    @Lob
+    private Blob ppt;
+    @Lob
+    private Byte report;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "projectStatus", length = 10)
+    private Status projectStatus;
 
 }
---
+++ stater/src/main/java/com/service/StudentService.java
@@ -0,0 +1,25 @@
+package com.service;
+
+public class StudentService {
+}
+
+//@Service
+//public class LobHelper {
+//
+//    private final SessionFactory sessionFactory;
+//
+//    @Autowired
+//    public LobHelper(SessionFactory sessionFactory) {
+//        this.sessionFactory = sessionFactory;
+//    }
+//
+//    public Blob createBlob(InputStream content, long size) {
+//        return sessionFactory.getCurrentSession().getLobHelper().createBlob(content, size);
+//    }
+//
+//    public Clob createClob(InputStream content, long size, Charset charset) {
+//        return sessionFactory.getCurrentSession().getLobHelper().createClob(new InputStreamReader(content, charset), size);
+//    }
+//}
+
+//https://stackoverflow.com/questions/31469136/spring-data-jpa-storing-blob
